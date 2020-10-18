38e7b5cd257de6f7f7a54b113b75b7b84199acc2
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2020-10-18 23:02:24 +0300

    Update Prob14.py

--- Prob14.py
+++ Prob14.py
@@ -1,9 +1,7 @@
 n=int(input("dati numarul lui Ion:"))
 if (n%4==0):
-    print("Ion va fi in casuta numarul:", n//4)
+    print("Ion va fi in casuta cu numarul:", n//4)
 elif (n%4)==1:
     print("Ion va fi in casuta cu numarul:",round(n/4)+1)
-elif (n%4)==2:
-    print("Ion va fi in casuta cu numarul:",round(n/4))
-else :
+elif (n%4)==2 or (n%4)==3 :
     print("Ion va fi in casuta cu numarul:",round(n/4))
