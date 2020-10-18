43b3367dd668e9b61ee08c2ec12fbe4f04f3d415
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2020-10-18 12:50:16 +0300

    Add files via upload

---
+++ Prob11.py
@@ -0,0 +1,15 @@
+a=int(input("Dati primul numar:"))
+b=int(input("Dati al doilea numar:"))
+c=int(input("Dati al treilea numar:"))
+if (a%2==0):
+    print(a,"par")
+else:
+    print(a,"impar")
+if (b%2==0):
+    print(b,"par")
+else:
+    print(b,"impar")
+if (c%2==0):
+    print(c,"par")
+else:
+    print(c,"impar")
---
+++ Prob12.py
@@ -0,0 +1,11 @@
+n=int(input("Introduceti numarul petatelor:"))
+if n% 5 == 1:
+    print("un pic")
+elif n% 5 ==  2:
+        print ("mult")    
+elif n% 5 ==  3:
+        print ("cu pasiune")
+elif n% 5 ==  4:
+        print("la nebunie")
+else:
+        print ("de loc")
